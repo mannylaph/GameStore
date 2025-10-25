829622d2dc8e31838891360f903f225d4d5bf9dc
Manny <Mannyiheukwumere@gmail.com>
2025-10-25 21:13:09 +0100

    Adding Games into the loop

--- GameStore.FrontEnd/obj/Debug/net9.0/GameStore.FrontEnd.AssemblyInfo.cs
+++ GameStore.FrontEnd/obj/Debug/net9.0/GameStore.FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c576e097842539eac887663740d8550bfb61279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45b2041f84b91320381a0635d51c04607e768a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
